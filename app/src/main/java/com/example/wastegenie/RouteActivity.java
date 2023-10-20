2db5d80590eac22e51719a921845538a88af4eb7
slam143 <103974999+slam143@users.noreply.github.com>
2023-10-21 01:58:18 +1100

    Added specific truck route activity, will test out the route plotting soon

---
+++ app/src/main/java/com/example/wastegenie/RouteActivity.java
@@ -0,0 +1,121 @@
+package com.example.wastegenie;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RouteActivity extends AppCompatActivity {
+
+    NavigationView navigationView;
+
+    DatabaseReference database;
+    ArrayList<String> addressList = new ArrayList<String>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_route);
+        setTitle("Route Tracking Page");
+
+
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://maps.googleapis.com/maps/api/directions/")
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        // Get all stops taken by a particular truck
+        database = FirebaseDatabase.getInstance().getReference().child("1qHYUHw1GGaVy9oW_pT8LMAWjR9fODaJE1qWqhcSNHBs").child("Sheet1");
+        // will change hardcoded value later
+        database.orderByChild("truckId").equalTo("PA122").addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                BinData bindata = snapshot.getValue(BinData.class);
+                String address = bindata.getBinAddress();
+                addressList.add(address);
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+
+        /* For creating the route:
+        https://developers.google.com/maps/documentation/directions/get-directions#DirectionsResponse
+        https://www.geeksforgeeks.org/how-to-draw-polyline-in-google-maps-in-android/
+         */
+
+
+
+        /**
+         * Set up navigation view
+         */
+        navigationView = findViewById(R.id.nav_view);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+
+                if(id == R.id.home) {
+                    Intent intent = new Intent(RouteActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                    return true;
+                } else if (id == R.id.analysis) {
+                    Intent intent = new Intent(RouteActivity.this, AnalysisActivity.class);
+                    startActivity(intent);
+                    finish();
+                    return true;
+                } else if (id == R.id.tracking) {
+                    return true;
+                } else if (id == R.id.profile) {
+                    Intent intent = new Intent(RouteActivity.this, ProfileActivity.class);
+                    startActivity(intent);
+                    finish();
+                    return true;
+                } else if (id == R.id.logout) {
+                    Toast.makeText(getApplication(), "Logout Selected", Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+
+                return false;
+            }
+        });
+    }
+}
